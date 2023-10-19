7a03b6bbb4c55338081ef6502538fc398d733031
ajjubisen <ajjubisen830@gmail.com>
2023-10-19 11:36:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/scrollingverticalhorizontal/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.scrollingverticalhorizontal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button = findViewById(R.id.btn)
+
+
+        button.setOnClickListener {
+            val intent = Intent(this, Horizontal::class.java)
+            startActivity(intent)
+        }
+
+    }
+    }
